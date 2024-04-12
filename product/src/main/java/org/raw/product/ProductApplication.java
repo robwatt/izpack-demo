b53f2785c46991572c3ed96c86d64eee2efedcbb
Rob Watt <rwatt@csod.com>
2024-04-12 16:37:54 -0400

    Work a multi-project version

---
+++ product/src/main/java/org/raw/product/ProductApplication.java
@@ -0,0 +1,13 @@
+package org.raw.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+}
